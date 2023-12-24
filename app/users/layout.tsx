291bd8c0b5c1203e9fa33cbe19e8abb3e4128b8f
olesia098 <olesya.kan.05@mail.ru>
2023-12-25 05:59:32 +0600

    error :,(

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -9,6 +9,7 @@ export default async function UserLayout({
 }) {
     const users = await getUsers()
     return(
+        // @ts-expect-error Server Component
         <Sidebar>
         <div className="h-full">
             <UserList items={users} />
