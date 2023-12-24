0bc57ea4ee64af4fb3f1f16a47681c8473adc707
olesia098 <olesya.kan.05@mail.ru>
2023-12-25 05:13:18 +0600

    finalgit add .

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import { use, useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
 import { User } from "@prisma/client";
-
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/modals/LoadingModal";
 
 interface UserBoxProps {
   data: User;
@@ -24,35 +24,38 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
       .finally(() => setIsLoading(false));
   }, [data, router]);
 
-  console.log("POIUYTREWSDFGHJK", data.id)
+  console.log("POIUYTREWSDFGHJK", data.id);
 
   return (
-    <div
-      onClick={handleClick}
-      className="
-            w-full 
-            relative 
-            flex 
-            items-center 
-            space-x-3 
-            bg-white 
-            p-3 
-            hover:bg-neutral-100
-            rounded-lg
-            transition
-            cursor-pointer
-          "
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <span className="absolute inset-0" aria-hidden="true" />
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="
+          w-full 
+          relative 
+          flex 
+          items-center 
+          space-x-3 
+          bg-white 
+          p-3 
+          hover:bg-neutral-100
+          rounded-lg
+          transition
+          cursor-pointer
+        "
+      >
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <span className="absolute inset-0" aria-hidden="true" />
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">{data.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
